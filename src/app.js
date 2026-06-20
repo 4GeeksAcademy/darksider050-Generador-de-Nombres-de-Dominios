9ca16d89e5caff16e79dca4bdee1b5a97d1df234
darksider050 <robertelias38@gmail.com>
2026-06-20 04:47:38 +0000

    generado funcional mostranto resultado requerido en pantalla sin css

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  
+  // 1. Buscamos el elemento donde vamos a mostrar los dominios
+  let listElement = document.getElementById("domain-list");
+
+  // 2. Recorremos los arrays
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        
+        // 3. Creamos un nuevo elemento <li> por cada combinación
+        let newLi = document.createElement("li");
+        newLi.innerText = `${p}${a}${n}.com`;
+        
+        // 4. Lo añadimos a la lista en el HTML
+        listElement.appendChild(newLi);
+      }
+    }
+  }
 };
